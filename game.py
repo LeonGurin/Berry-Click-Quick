e62157f26796460a975b45a13e69b47886e56363
Leon Gurin <leon9gur@gmail.com>
2022-02-23 00:07:42 +0200

    added moving backgroup

--- game.py
+++ game.py
@@ -1,12 +1,9 @@
+from tkinter.tix import MAIN
 import pygame
 import random
 import os
 import sys
 import time
-
-pygame.init()
-
-# fixed python 32-bit bug 
 
 from pygame.locals import (
     K_UP,
@@ -20,19 +17,49 @@ from pygame.locals import (
     RLEACCEL,
 )
 
+pygame.init()
+
+# fixed python 32-bit bug 
+# add score bar and timer
+# add sound
+# add game over
+# add high score
+# add pause
+
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
-#set game tic rate
 FPS = 30
 
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pic = pygame.image.load(os.path.dirname(__file__) + "\\background.jpg").convert()
-
+# init screen and clock
 pygame.display.set_caption('Clicky Bicky Game')
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 clock.tick(FPS)
 
+# init background
+class Background(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.dirname(__file__) + "\\sky2.jpg").convert()
+        self.image = pygame.transform.scale(self.image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.rect = self.image.get_rect()
+        self.x = 0
+        self.y = 0
+        self.y2 = self.rect.height
+
+    def update(self):
+        self.y -= 0.05
+        self.y2 -= 0.05
+        if self.y <= -self.rect.height:
+            self.y = self.rect.height
+        if self.y2 <= -self.rect.height:
+            self.y2 = self.rect.height
+
+    def render(self):
+        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.image, (self.x, self.y2))
+
+# init text
 font = pygame.font.Font('freesansbold.ttf', 32)
 text = font.render('Clicky Bicky Game', True, (0,255,0), (0,0,255))
 textRect = text.get_rect()
@@ -42,7 +69,7 @@ textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 200)
 class Fruit(pygame.sprite.Sprite):
     def __init__(self):
         super(Fruit, self).__init__()
-        self.surf = pygame.image.load(os.path.dirname(__file__) + "\\Strawberry.png").convert()
+        self.surf = pygame.image.load(os.path.dirname(__file__) + "\\Strawberry.png").convert_alpha()
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         # self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
@@ -54,11 +81,13 @@ class Fruit(pygame.sprite.Sprite):
             if y > self.posY and y < self.posY + self.rect.height:
                 # self.surf.fill((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
                 self.posX = random.randint(0, SCREEN_WIDTH - self.rect.width)
-                self.posY = random.randint(60, SCREEN_HEIGHT - self.rect.height)
+                self.posY = random.randint(100, SCREEN_HEIGHT - self.rect.height)
         
-
+# init objects
+bg = Background()
 fruit = Fruit()
 
+# game loop
 running = True
 while running:    
     for event in pygame.event.get():
@@ -68,17 +97,19 @@ while running:
             if event.key == K_ESCAPE:
                 running = False
 
-    print(pygame.mouse.get_pos())
+    #print(pygame.mouse.get_pos())
 
     #if the mouse is clicked
     if event.type == MOUSEBUTTONDOWN:
         (x,y) = pygame.mouse.get_pos()
         fruit.is_clicked(x,y)
     
-    screen.blit(pygame.transform.scale(pic, (1000, 500)), (0, 0))
+    bg.render()
+    bg.update()
     screen.blit(text, textRect)
     screen.blit(fruit.surf, (fruit.posX, fruit.posY))
     pygame.display.flip()
     
 
 pygame.quit()
+sys.exit()
